a7bf2edbaf80267c447949582d1609ee742b55f6
blacktea13 <blacktea@RobBox>
2022-11-18 16:56:27 +0700

    changed district to zip in schema

--- tablescript.sql
+++ tablescript.sql
@@ -1,9 +1,20 @@
 CREATE TABLE customers (
-    customer_id int auto_increment primary key, 
-    first_name varchar(20) null
-    last_name varchar(20) null
-    date_of_birth() date,
+    customer_id int auto_increment,
+    first_name varchar(20) null,
+    last_name varchar(20) null,
+    date_of_birth date,
+    email_address varchar(320),
     address_line varchar(100),
     district varchar(20),
-    city varchar(20)
-)
+    city varchar(20),
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE employees (
+    employee_id int auto_increment,
+    first_name varchar(20),
+    last_name varchar(20),
+    date_of_birth date,
+    email_address varchar(320),
+    PRIMARY KEY (employee_id)
+);
