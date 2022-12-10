1608e347d7fd5da912b64f78f382306dabc6e3a3
BlackTea13 <53616626+BlackTea13@users.noreply.github.com>
2022-12-10 17:44:58 +0700

    added some constraints to tablescript.fixed bug by changing order of table creation

--- tablescript.sql
+++ tablescript.sql
@@ -20,21 +20,21 @@ CREATE TABLE city_country(
 DROP TABLE IF EXISTS time_slot;
 CREATE TABLE time_slot(
     time_slot_id int,
-    work_day char(10),
-    start_time time,
-    end_time time,
+    work_day char(10) NOT NULL,
+    start_time time NOT NULL,
+    end_time time NOT NULL,
     PRIMARY KEY (time_slot_id, work_day, start_time, end_time)
 );
 
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
     customer_id int auto_increment,
-    first_name varchar(20) null,
-    last_name varchar(20) null,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL,
     date_of_birth date,
-    email_address varchar(320),
-    address_line varchar(600),
-    zip int,
+    email_address varchar(320) NOT NULL UNIQUE,
+    address_line varchar(600) NOT NULL,
+    zip int NOT NULL,
     city varchar(40),
     PRIMARY KEY (customer_id),
     FOREIGN KEY (zip) REFERENCES district_zip(zip),
@@ -44,13 +44,13 @@ CREATE TABLE customers (
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
     employee_id int auto_increment,
-    first_name varchar(20),
-    last_name varchar(20),
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL,
     date_of_birth date,
-    email_address varchar(320),
-    time_slot_id int,
-    address_line varchar(600),
-    zip int,
+    email_address varchar(320) NOT NULL UNIQUE,
+    time_slot_id int NOT NULL,
+    address_line varchar(600) NOT NULL,
+    zip int NOT NULL,
     city varchar(20),
     PRIMARY KEY (employee_id),
     FOREIGN KEY (zip) REFERENCES district_zip(zip),
@@ -61,8 +61,8 @@ CREATE TABLE employees (
 DROP TABLE IF EXISTS suppliers;
 CREATE TABLE suppliers(
     supplier_id int auto_increment,
-    telephone_number varchar(15),
-    email_address varchar(320),
+    telephone_number varchar(15) NOT NULL UNIQUE,
+    email_address varchar(320) NOT NULL UNIQUE,
     address_line varchar(600),
     zip int,
     city varchar(20),
@@ -74,8 +74,8 @@ CREATE TABLE suppliers(
 DROP TABLE IF EXISTS menu;
 CREATE TABLE menu(
     product_id int auto_increment,
-    product_name varchar(600),
-    price_per_unit float,
+    product_name varchar(600) NOT NULL,
+    price_per_unit float NOT NULl,
     PRIMARY KEY (product_id)
 
 );
@@ -84,7 +84,7 @@ DROP TABLE IF EXISTS orders;
 CREATE TABLE orders(
     order_id int auto_increment,
     customer_id int,
-    order_date date,
+    order_date date NOT NULL,
     PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
@@ -93,7 +93,7 @@ DROP TABLE IF EXISTS order_line;
 CREATE TABLE order_line(
     order_id int,
     product_id int,
-    quantity int,
+    quantity int NOT NULL,
     PRIMARY KEY (order_id, product_id),
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES menu(product_id)
@@ -128,7 +128,7 @@ DROP TABLE IF EXISTS job;
 CREATE TABLE job(
     job_id int auto_increment,
     job_name varchar(100),
-    salary numeric(15,2),
+    salary numeric(15,2) NOT NULL,
     PRIMARY KEY (job_id)
 );
 
@@ -141,27 +141,24 @@ CREATE TABLE positions(
     FOREIGN KEY (job_id) REFERENCES job(job_id)
 );
 
-DROP TABLE IF EXISTS users;
-create table users
-(
-    user_id    int auto_increment,
-    first_name varchar(30)  null,
-    last_name  varchar(30)  null,
-    username   varchar(20)  unique,
-    email      varchar(30)  null,
-    password   varchar(128) null,
-    role_id    int          null,
-    PRIMARY KEY (user_id)
-    FOREIGN KEY (role_id) REFERENCES roles(role_id),
-);
-
 DROP TABLE IF EXISTS roles;
-create table roles
+CREATE TABLE roles
 (
     role_id int,
-    role_description varchar(30),
-    PRIMARY KEY (role_id),
-    
+    role_description varchar(30) NOT NULL,
+    PRIMARY KEY (role_id)
 );
 
-
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
+(
+    user_id    int auto_increment,
+    first_name varchar(30),
+    last_name  varchar(30),
+    username   varchar(20)  NOT NULL UNIQUE,
+    email      varchar(30)  NOT NULL UNIQUE,
+    password   varchar(128) NOT NULL,
+    role_id    int          NOT NULL,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
